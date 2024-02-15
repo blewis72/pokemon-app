dfa8182b5ede04030d4bef2b79f67fc5c6c0a761
dorantelilly <157527046+dorantelilly@users.noreply.github.com>
2024-02-15 18:00:21 -0500

    Update home.js
    pokemon carousel

--- src/home.js
+++ src/home.js
@@ -1,29 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
-
-const Home = ({ pokemonProp: results }) => {
-    console.log(results);
+const Home = ({ pokemonProp }) => {
+    const sliderRef = useRef(null);
+  
+    const settings = {
+      dots: false,
+      infinite: false,
+      speed: 700,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    };
+  
+    const nextSlide = () => {
+      if (sliderRef.current) {
+        sliderRef.current.slickNext();
+      }
+    };
+  
+    const prevSlide = () => {
+      if (sliderRef.current) {
+        sliderRef.current.slickPrev();
+      }
+    };
+  
+    useEffect(() => {
+        // Reinitialize the slider when filtered data changes
+        if (sliderRef.current) {
+          sliderRef.current.slickGoTo(0); // Go to the first slide
+          sliderRef.current.slickPrev(); // Move to the previous slide (to reinitialize)
+        }
+      }, [pokemonProp]);
+  
     return (
-       
-
-        <div className="flex">
-    
-
-            {results &&
-                results.map((val, index) => (
-                    <div className="ml-4 text-2xl text-yellow-400 border-black-500 " key={val.index}>
-                        <Link to={`/about/${val.index}`}>{val.name}</Link>
+      <div>
+        <h2>List of Pokemon</h2>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <button onClick={prevSlide}>&#9664;</button>
+          <div style={{ width: '70%' }}>
+            <Slider {...settings} ref={sliderRef}>
+              {pokemonProp.map((pokemon) => (
+                <div key={pokemon.index} style={{ textAlign: 'center' }}>
+                  <Link to={`/about/${pokemon.index}`}>
+                    <div>
+                      <img
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.index}.png`}
+                        alt={pokemon.name}
+                      />
+                      <p>{pokemon.name}</p>
                     </div>
-
-                ))
-
-            }
-
-
+                  </Link>
+                </div>
+              ))}
+            </Slider>
+          </div>
+          <button onClick={nextSlide}>&#9654;</button>
         </div>
-
-    )
-}
-
-export default Home;
+      </div>
+    );
+  };
+  
+  export default Home;
