031891d472140e62ca0a259d8399abd44cee91df
dorantelilly <157527046+dorantelilly@users.noreply.github.com>
2024-02-16 10:50:55 -0500

    Update about.js with back button that renders only on about page

--- src/about.js
+++ src/about.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import styles from "./pokemon.module.css";
+import {useParams, Link, useLocation} from 'react-router-dom';
 
 function About (props) {
     let {pokemonId} = useParams();
-
+    const location = useLocation();
     const [pokemon,setPokemon]=useState();
 
     useEffect(() => {
@@ -20,28 +19,49 @@ function About (props) {
         }   
     },[pokemonId]);
     return(
-        <div >
+        < >
+       {location.pathname !== '/about/:pokemonId' && (
+        <Link to="/">
+          <button
+            style={{
+              position: 'absolute',
+              top: '20px',
+              left: '20px',
+              backgroundColor: '#0F53A3',
+              color: '#EDCC20',
+              paddingLeft: '20px',
+              paddingRight: '20px',
+              paddingTop: '5px',
+              paddingBottom: '5px',
+              borderRadius: '5px',
+              marginLeft: '60px',
+              marginTop: '20px',
+            }}
+          >
+            Back
+          </button>
+        </Link>
+      )}
+
         {pokemon && (
-        <div className={styles.PokemonImage}>
-          
+        <div>
+          <h1>{pokemon.name}</h1>
 
           <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-          {/* <img src={pokemon.sprites.front_shiny} alt={pokemon.name} /> */}
+          <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
 
-          
-          <h1>{pokemon.name}</h1>
           <h2>Type: {pokemon.types.map((type, index) => (
             <div key={index}>{type.type.name}</div>
           ))}</h2>
-       
           
           <h2>Stats: {pokemon.stats.map((stat, index) => (  
             <div key={index}>{stat.stat.name}: {stat.base_stat}</div>
           ))}</h2>
 
+            
         </div>
       )}
-        </div>
+        </>
     )
 }
 export default About;
