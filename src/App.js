225ea04642a0b51655f646b019f7cd8825613798
dorantelilly <157527046+dorantelilly@users.noreply.github.com>
2024-02-16 10:15:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
 import './App.css';
-import About from './about';
-import Home from './home';
 import { Routes, Route } from "react-router"
 import { BrowserRouter } from "react-router-dom"
+import About from './About';
+import Home from './Home';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 
 
 
@@ -14,10 +12,6 @@ const App = () => {
   const [pokemonData, setPokemonData] = useState([]);
   const [inputSearch, setInputSearch] = useState([]);
   const [filteredPokemon, setFilteredPokemon] = useState([]);
-
-  
-  //clear search and refresh the page
-  
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=30")
@@ -33,32 +27,40 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-  
     setFilteredPokemon(() =>
       pokemonData.results?.filter((pokemon) => pokemon.name.includes(inputSearch)))
   }, [pokemonData.results, inputSearch]);
+
+ 
+
   return (
     <BrowserRouter>
     
+    <div className='flex px-0 py-5 w-full bg-red-600 rounded-sm shadow-sm max-md:px-5 max-md:max-w-full' >
+    <Link to="/">
+          <button style={{ backgroundColor: '#0F53A3', color: '#EDCC20', paddingLeft: '20px', paddingRight: '20px', paddingTop: '5px', paddingBottom:'5px', borderRadius: '5px', marginLeft:'60px', marginTop:'20px'}}>
+            Back
+          </button>
+        </Link>
 
-      <div className='flex justify-center items-center px-16 py-5 w-full bg-red-600 rounded-sm shadow-sm max-md:px-5 max-md:max-w-full'>
-       <Link to="" ><img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&"
-          className="max-w-full aspect-[2.94] w-[200px]"
-        />
-        </Link> 
-      </div>
-     
+     <Link to="" ><img style={{marginLeft: '470px', justifyContent: 'center', alignItems: 'center', display: 'flex'}}
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/7de3c097122ff0c4b54e4dfa20e8cfdda9158c45b60ef0071a5e00e87ac2f619?apiKey=3f24d196149340fe82d51ab9618c65c0&"
+        className="max-w-full aspect-[2.94] w-[200px]"
+      />
+      </Link> 
+      
+    </div>
+   
 
-      <div className='p-6'>
-        <div className='flex flex-col items-center'>
+    <div className='p-6'>
+      <div className='flex flex-col items-center'>
+       
+          <header className='text-4xl text-black-700'> Choose your favorite pokemon</header>
          
-            <header className='text-4xl text-black-700'> Choose your favorite pokemon</header>
-           
-        </div>
       </div>
-      <div className='w- full flex justify-center' style={{height: 70}}>
+    </div>
+    <div className='w- full flex justify-center' style={{height: 70}}>
       <input onChange={(e) => setInputSearch(e.target.value)}
         placeholder='Explore'
         type='text'
@@ -70,9 +72,8 @@ const App = () => {
     srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/743c724135eb977a5c23d69aafb605ff51649dc8b4cfa5a229dafc0c7ef0efb9?apiKey=3f24d196149340fe82d51ab9618c65c0&"
     className="self-center aspect-[1.02] w-[82px]"
   />
-     </Link>
-    </button>
-   
+   </Link>
+  </button>
       </div>
 
       <Routes>
@@ -86,4 +87,3 @@ const App = () => {
   );
 };
 export default App;
-
